0719a4dd5d21faa48fd812b8c224369ca961ad70
Arigaran <arigaran.sundaram@gamil.com>
2023-02-22 20:39:24 +0530

    advance

---
+++ first/src/main/java/servlet/req/servlet2.java
@@ -0,0 +1,29 @@
+package servlet.req;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public servlet2() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw=response.getWriter();
+		pw.print("  welcome to inbox");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
